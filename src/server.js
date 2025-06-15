c52e53875115102127c80f67c7e8c5a1855b56db
satweekg <satweekg.shivinfotech@gmail.com>
2025-06-16 03:34:42 +0530

    add large size

--- src/server.js
+++ src/server.js
@@ -18,7 +18,8 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '200mb' }));
+
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Request logging middleware
