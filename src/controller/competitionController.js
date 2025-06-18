a2781a205790ceb6f775376d2e394c6a68be406d
satweekg <satweekg.shivinfotech@gmail.com>
2025-06-18 10:55:40 +0530

    mak bol

--- src/controller/competitionController.js
+++ src/controller/competitionController.js
@@ -334,7 +334,7 @@ function generateCertificateHTML(registration) {
                     <!-- Main Content -->
                     <div class="content-section">
                         <div class="participation-details">
-                            for winning the ${registration.grade_or_winning_rank} position in the <br> WSRO IRAN National Competition 2025 on the date
+                            for winning the <strong> ${registration.grade_or_winning_rank}</strong> position in the <br> WSRO IRAN National Competition 2025 on the date
                             of <strong>21,22 & 23 May 2025</strong>
                             <br>At <strong>${registration.school_institute}.</strong>
                         </div>
